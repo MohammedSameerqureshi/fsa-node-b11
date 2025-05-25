2ca144ebc9c9f78ab91b1af7d5f727443e000b23
MohammedSameerqureshi <msqureshi2803@gmail.com>
2025-05-25 13:02:10 +0530

    req/res Controller

--- .vscode/backend/firstprj/app.js
+++ .vscode/backend/firstprj/app.js
@@ -1,25 +1,19 @@
-import http from "http"
-import path from "path"
-// const require = require("http");
-import "./module.js"
+import http from "http";
+// nodeconst require = require("http");
 
-const server = http.createServer((req, res) => {
-  console.log(path);
-// const controller = (req , res) => {
-    // console.log(req.url);
-    //  res.statusCode = 400;
-    // res.end("hello world")
-
-   if(req.url.includes("welcome")) {
-      res.statusCode = 200;
-      res.end("Welcome to my backend server");
-   } else {
-    res.statusCode = 404;
-    
-
-    res.end()
-   }
-});
+ const server = http.createServer((req, res) => {
+    //   console.log(req.body);
+    // res.statusCode = 400;
+    // res.end("hello world");
+    if (req.url.includes("Welcome")) {
+        res.statusCode = 200;
+        res.end("Welcome to my Backend Server");
+    } else {
+        res.statusCode = 404;
+        res.end();
+    }
+ }) ;
 
 
-server.listen(5000, () => console.log("Server is UP & Running"));
+
+server.listen(5000, () => console.log("Server UP & Running"));
